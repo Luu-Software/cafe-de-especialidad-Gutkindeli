4c4857e11cc5d1a52b07ed57d11ca56dfcefc502
Gutkindeli <gutkindeli@gmail.com>
2026-05-26 12:15:00 -0300

    ft

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import { preguntar } from './lib/consola.ts';
 let totalTexto: string = preguntar('Total de la cuenta: ');
 let esEstudiante: string = preguntar('¿Son estudiantes de ORT? si/no: ');
 let personasTexto: string = preguntar('Dividir la cuenta entre: ');
-
+let cuenta: number= Number(totalTexto);
 let totalPorPersona: number = 0; // COMPLETAR
 console.log(`Total por persona: $${totalPorPersona}`);

c137f87909d1a5a88023de43ccdb729755bc28f0
Gutkindeli <gutkindeli@gmail.com>
2026-05-27 15:58:34 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,17 @@ let totalTexto: string = preguntar('Total de la cuenta: ');
 let esEstudiante: string = preguntar('¿Son estudiantes de ORT? si/no: ');
 let personasTexto: string = preguntar('Dividir la cuenta entre: ');
 let cuenta: number= Number(totalTexto);
-let totalPorPersona: number = 0; // COMPLETAR
+let totalPorPersona: number = 0; 
+
+if (esEstudiante === "si"){
+
+    totalPorPersona =  Number(totalTexto) * 0.9 / Number(personasTexto);
+}
+else {
+
+    totalPorPersona =  Number(totalTexto)/ Number(personasTexto);
+
+}
+
+// COMPLETAR
 console.log(`Total por persona: $${totalPorPersona}`);
